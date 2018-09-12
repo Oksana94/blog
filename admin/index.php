fbffc8f5633b73fddf7879d6aaac98fa87aed37f
Test <volyinets@itstep1.dp.ua>
2018-09-12 20:03:26 +0300

    commit 1

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volyinets
+ * Date: 12.09.2018
+ * Time: 19:19
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+    <script src="./jquery-3.3.1.min.js"></script>
+    <script src="./script.js"></script>
+    <title>Admin</title>
+</head>
+<body>
+<nav class="menu">
+    <a href="#" class="logo">Panda</a>
+    <ul>
+        <li class="item active"><a href="">Записи</a></li>
+        <li class="item"><a href="">Медифайлы</a></li>
+        <li class="item"><a href="">Страница</a></li>
+    </ul>
+    <a href="#" class="logOut">Выход</a>
+</nav>
+<div class="wrap">
+
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volyinets
+ * Date: 12.09.2018
+ * Time: 19:12
+ */
+//$route = [
+//    "default"=>"pages/main.php",
+//    "gallery"=>"pages/gallery.php",
+//    "about"=>"pages/about.php",
+//    "title"=>"pages/title.php"
+//];
+//
+//$page = @$_GET["page"];
+
+require_once 'modules/header.php';
+
+?>
+<div class="wrap">
+<?php
+//    if(empty($page)) include $route["default"];
+//    else{
+//        if(isset($route[$page])) include $route[$page];
+//        else echo "<h1>404</h1>";
+//    }
+//    ?>
+</div>
+<?php
+require_once 'modules/footer.php';
+?>
